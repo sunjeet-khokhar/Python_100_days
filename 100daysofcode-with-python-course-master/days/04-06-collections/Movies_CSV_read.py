2ce3a5e01033798817d803995a06fa269431cb6e
sunjeet-khokhar <sunjeet81@gmail.com>
2018-04-12 16:53:46 +1200

    Added a program to parse a csv using a named tuples and default dicts

--- 100daysofcode-with-python-course-master/days/04-06-collections/Movies_CSV_read.py
+++ 100daysofcode-with-python-course-master/days/04-06-collections/Movies_CSV_read.py
@@ -3,11 +3,9 @@ from collections import namedtuple
 from collections import defaultdict
 
 Movie = namedtuple('movie','name year imdb_score')
-movie_details = []
 movie_list = defaultdict(list)
 with open('movie_metadata.csv',encoding='utf-8',mode='r') as movies_csv:
     for row in csv.DictReader(movies_csv):
-        #print(row)
         director = row['director_name']
         name = row['movie_title'].replace('\xa0','')
         year = row['title_year']
